3aa7e0ccd4545e891c3c0fd3b8a3c91ff26cdce8
Anurag Pandey <anurag.ppandey@flobiz.in>
2025-12-18 20:50:09 +0530

    Initial commit

---
+++ src/main/java/com/coinShiftProject/coinShiftProject/CoinShiftProjectApplication.java
@@ -0,0 +1,13 @@
+package com.coinShiftProject.coinShiftProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinShiftProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinShiftProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coinShiftProject/coinShiftProject/CoinShiftProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinShiftProject.coinShiftProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinShiftProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
